22452f474aefc30e050772f1a22380b1115acd9f
Xavier Devroey <xavier.devroey@gmail.com>
2019-10-15 14:11:00 +0200

    Including model-statistics tool in the toolbox + migrate travis file to openjdk8

--- vibes-toolbox/toolbox-model-statistics/src/main/java/be/unamur/info/toolbox/modelstatistics/Main.java
+++ vibes-toolbox/toolbox-model-statistics/src/main/java/be/unamur/info/toolbox/modelstatistics/Main.java
@@ -2,8 +2,8 @@ package be.unamur.info.toolbox.modelstatistics;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import static be.unamur.transitionsystem.dsl.ModelStatisticsPrinter.*;
-import static be.unamur.transitionsystem.dsl.TransitionSystemXmlLoaders.*;
+import static be.vibes.dsl.ts.ModelStatisticsPrinter.*;
+import static be.vibes.dsl.io.Xml.*;
 
 /**
  *
@@ -12,15 +12,15 @@ import static be.unamur.transitionsystem.dsl.TransitionSystemXmlLoaders.*;
 public class Main {
 
     public static void main(String[] args) {
-        checkArgument(args.length >= 2, "Wrong number of arguments: java -jar toolbox-model-statistics.jar (-lts <model.lts> | -fts <model.fts> | -um <model.um>)");
-        if (args[0].equals("-lts")) {
-            printModelStatistics(loadLabelledTransitionSystem(args[1]));
+        checkArgument(args.length >= 2, "Wrong number of arguments: java -jar toolbox-model-statistics.jar (-ts <model.ts> | -fts <model.fts> | -um <model.um>)");
+        if (args[0].equals("-ts")) {
+            printModelStatistics(loadTransitionSystem(args[1]));
         } else if (args[0].equals("-fts")) {
             printModelStatistics(loadFeaturedTransitionSystem(args[1]));
         } else if (args[0].equals("-um")) {
             printModelStatistics(loadUsageModel(args[1]));
         } else {
-            throw new IllegalArgumentException("Wrong arguments: java -jar toolbox-model-statistics.jar (-lts <model.lts> | -fts <model.fts> | -um <model.um>)");
+            throw new IllegalArgumentException("Wrong arguments: java -jar toolbox-model-statistics.jar (-ts <model.ts> | -fts <model.fts> | -um <model.um>)");
         }
     }
     
